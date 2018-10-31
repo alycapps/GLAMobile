c3ee5edc76940c6cf0b4e0856e78d3fee8899b80
DjangoJuice <dungeonmesher@gmail.com>
2018-10-30 21:20:19 -0400

    Appointment Schema & seeds

---
+++ client/src/pages/Client/Client (mike-laptop's conflicted copy 2018-10-30).js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import ClientInfo from "../../components/Client";
+
+// import { Input, TextArea, FormBtn } from "../../components/Form";
+
+// const styles = {
+//   primaryColor: {
+//     background: `#fbe9e7`,
+//   },
+//   secondaryColor: {
+//     background: `#c8b7b5`,
+//   },
+//   anotherColor: {
+//     background: `#ffffff`,
+//   }
+// }
+
+class Books extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: "",
+    client: JSON.parse(localStorage.getItem('client'))
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <form>
+              <h3>Upcoming Appointments</h3>
+              {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Appointments Scheduled</h3>
+            )}
+              <Link to="/search">
+                <button className="btn btn-success"> 
+                  Book a New Appointment
+                </button>
+              </Link>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <ClientInfo />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
