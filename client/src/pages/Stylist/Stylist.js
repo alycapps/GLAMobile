259d804df14230f90587771327d37b42abe2afbc
thelaceyedit <thelaceyedit@gmail.com>
2018-11-01 21:29:11 -0400

    stylist pricing

--- client/src/pages/Stylist/Stylist.js
+++ client/src/pages/Stylist/Stylist.js
@@ -14,46 +14,90 @@ class Books extends Component {
   render() {
     return (
       <Container fluid>
-      <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">Stylist</span>
-      </nav>
+      <ul className="nav nav-pills nav justify-content-center"  margin-top="20px">
+        <li className="nav-item">
+          <a className="nav-link active" href="/Calendar">View My Scheduled Appointments</a>
+        </li>
+      </ul>
         <Row>
           <Col size="md-6">
-            <Jumbotron>
-            <form>
-            <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Stylist Pricing
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a className="dropdown-item" href="#">$25 - $50</a>
-              <a className="dropdown-item" href="#">$50 - $75</a>
-              <a className="dropdown-item" href="#">$75 - 100</a>
-              <a className="dropdown-item" href="#">$100 - $150</a>
-            </div>
-          </div>
-            </form>
-          </Jumbotron>
-          
           </Col>
           <Col size="md-6 sm-12">
-            <Jumbotron>
-            <Link to="/Calendar">
-                <button className="btn btn-success"> 
-                  View My Scheduled Appointments
-                </button>
-              </Link>
-            </Jumbotron>
+           <Container>
+           <text textAlign="right">Google Maps Goes Here</text>
+           </Container>
+
           </Col>
         </Row>
         <Row>
         <Col size="md-6 sm-12">
         <Container>
-       
+        <div className="card">
+        <form>
+        <div className="form-row">
+          <div className="col">
+            <input type="text" className="form-control" placeholder="License Number">
+            </input>
+          </div>
+          <div className="col">
+            <input type="text" className="form-control" placeholder="State">
+            </input>
+          </div>
+          </div>
+        </form>
+        </div>
         </Container>
+        <Container>
+        <row>
+        <form>
+        <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Hair
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a className="dropdown-item" href="#">$25 - $50</a>
+          <a className="dropdown-item" href="#">$50 - $75</a>
+          <a className="dropdown-item" href="#">$75 - 100</a>
+          <a className="dropdown-item" href="#">$100 - $150</a>
+        </div>
+      </div>
+        </form>
+        <br/>
+        <form>
+        <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Makeup
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a className="dropdown-item" href="#">$25 - $50</a>
+          <a className="dropdown-item" href="#">$50 - $75</a>
+          <a className="dropdown-item" href="#">$75 - 100</a>
+          <a className="dropdown-item" href="#">$100 - $150</a>
+        </div>
+      </div>
+        </form>
+        <br/>
+        <form>
+        <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Nails
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a className="dropdown-item" href="#">$25 - $50</a>
+          <a className="dropdown-item" href="#">$50 - $75</a>
+          <a className="dropdown-item" href="#">$75 - 100</a>
+          <a className="dropdown-item" href="#">$100 - $150</a>
+        </div>
+      </div>
+        </form>
+        <Row/>
+        <br/>
+        </row>
+      </Container>
         </Col>
         </Row>
       </Container>
+
     );
   }
 }
