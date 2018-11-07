e543b4bc25deb7606f67a5fdea8a2d58cc2edb5f
Aly <38168385+alycapps@users.noreply.github.com>
2018-11-06 19:26:01 -0500

    zachs fix for client not found

--- client/src/components/Client/ClientInfo.js
+++ client/src/components/Client/ClientInfo.js
@@ -1,28 +1,33 @@
 import React from "react";
 
-this.state = {
-  client: JSON.parse(localStorage.getItem('client'))
-}
-const ClientInfo = (props) => {
-  return (
-    <div>
-    <h3>My Profile</h3>
-    First Name: {this.state.client.firstName}
-    <br></br>
-    Last Name: {this.state.client.lastName}
-    <br></br>
-    Username: {this.state.client.username}
-    <br></br>
-    Adress: {this.state.client.address}
-    <br></br>
-    Apt: {this.state.client.apt}
-    <br></br>
-    City: {this.state.client.city}
-    <br></br>
-    State: {this.state.client.state}
-    <br></br>
-  </div>
-  )
-};
+class ClientInfo extends React.Component { 
+  state = {
+    client: JSON.parse(localStorage.getItem('client'))
+  }
 
+  render() {
+    if (!this.state.client) {
+      return <p>No Client found</p>
+    }
+    return (
+      <div>
+      <h3>My Profile</h3>
+      First Name: {this.state.client.firstName}
+      <br></br>
+      Last Name: {this.state.client.lastName}
+      <br></br>
+      Username: {this.state.client.username}
+      <br></br>
+      Adress: {this.state.client.username}
+      <br></br>
+      Apt: {this.state.client.username}
+      <br></br>
+      City: {this.state.client.username}
+      <br></br>
+      State: {this.state.client.username}
+      <br></br>
+    </div>
+    )  
+  }
+}
 export default ClientInfo;
