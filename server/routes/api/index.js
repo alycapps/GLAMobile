2d3746dde90b8670b1241948ef62e5e8752a3ca8
Aly <38168385+alycapps@users.noreply.github.com>
2018-11-08 13:46:38 -0500

    initial backend file setup for appts

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 const stylistRoutes = require("./users");
 
-
 // Book routes
 router.use("/books", bookRoutes);
 // Stylist routes
 router.use("/users/stylists", stylistRoutes);
+// Appt routes
+router.use("/appts", stylistRoutes);
 
 module.exports = router;
