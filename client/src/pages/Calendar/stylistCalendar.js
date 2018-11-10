eea419ded2ef7b320e312eb8aaaf7c12f48fd86f
Aly <38168385+alycapps@users.noreply.github.com>
2018-11-10 14:34:08 -0500

    fixed calendar functions

--- client/src/pages/Calendar/stylistCalendar.js
+++ client/src/pages/Calendar/stylistCalendar.js
@@ -72,19 +72,17 @@ class Calendar extends React.Component {
     while (day <= endDate) {
       for (let i = 0; i < 7; i++) {
         formattedDate = dateFns.format(day, dateFormat);
-        
         const cloneDay = day;
         days.push(
           <div
-            className="col cell"> {
+            className={`col cell ${
               !dateFns.isSameMonth(day, monthStart)
                 ? "disabled"
-                : dateFns.isSameDay(day, selectedDate) && "selected"
-              }
-            
+                : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
+            }`}
             key={day}
             onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
-
+          >
             <span className="number">{formattedDate}</span>
             <span className="bg">{formattedDate}</span>
           </div>
