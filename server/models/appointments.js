07d1be7425b5f6b7dbc6e4319566f73d9987f895
Aly <38168385+alycapps@users.noreply.github.com>
2018-11-12 16:09:10 -0500

    fixed schema for appts

--- server/models/appointments.js
+++ server/models/appointments.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const appointmentsSchema = new Schema({
   service: { type: String, required: false },
+  address: { type: String, required: false },
   city: { type: String, required: false },
   zipCode: { type: String, required: false },
-  dateTime: { type: String, required: false },
+  month: { type: String, required: false },
+  day: { type: Number, required: false },
+  year: { type: Number, required: false },
+  time: { type: String, required: false },
   clientId: {type: Schema.Types.ObjectId, ref: 'User'},
   stylistId: {type: Schema.Types.ObjectId, ref: 'User'}
 });
